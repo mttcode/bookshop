416bcf9106174588f6fb51a7f8708e62df74e37e
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2020-06-03 22:23:16 +0200

    Create OrderDetail.cs

---
+++ Models/OrderDetail.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop.Models
+{
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+        public int BookId { get; set; }
+        public Books Book { get; set; }
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+        public Order Order { get; set; }
+    }
+}
