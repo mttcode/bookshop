47343df9275f83344d86f36addd210456caef4bc
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2020-06-03 22:19:47 +0200

    Create CategoryRepository.cs

---
+++ Models/CategoryRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Category> GetAllCategories => _appDbContext.Categories;
+    }
+}
